fd78f6d28c6f111a5ffed1bc0c99db3fefff777c
Sheikh Fahim Faysal Sowrav <gfahim38@gmail.com>
2022-12-02 20:32:09 +0600

    2nd batch

---
+++ main.py
@@ -0,0 +1,13 @@
+b ="  Hello, world!"
+print(b[2:5])
+print(b[:5])
+print(b[2:])
+print(b[-5:-2])
+print(b.upper())
+print(b.lower())
+print(b.strip())
+print(b.replace("H","J"))
+print(b.split(","))
+
+
+
